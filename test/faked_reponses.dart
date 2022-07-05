1f7a099d74441eb2d9b93a0274fc2ea6aa847eb2
monaAgamal <mona.gamal1.mohamed.96@gmail.com>
2022-07-05 11:45:36 +0200

    [ADD] more tests

---
+++ test/faked_reponses.dart
@@ -0,0 +1,9 @@
+import 'package:movies_peaple/features/popular_persons/data/models/popular_people.dart';
+
+final PopularPersonsApiResponse fakePopularPersonsApiResponse =
+    PopularPersonsApiResponse(
+  page: 1,
+  results: [],
+  totalPages: 120,
+  totalResults: 1200,
+);
--- test/features/persons_list/data/persons_list_remote_data_source_test.dart
+++ test/features/persons_list/data/persons_list_remote_data_source_test.dart
@@ -3,15 +3,27 @@ import 'package:mockito/mockito.dart';
 import 'package:movies_peaple/features/popular_persons/data/api_service/api_service.dart';
 import 'package:movies_peaple/features/popular_persons/data/data_sources/popular_persons_remote_data_source_impl.dart';
 import 'package:flutter_test/flutter_test.dart';
+import '../../../faked_reponses.dart';
 import 'persons_list_remote_data_source_test.mocks.dart';
 
 @GenerateMocks([PopularPeopleApiService])
 void main() {
-  // Create mock object.
   var apiService = MockPopularPeopleApiService();
   final popularPersonsImpl = PopularPersonsRemoteDataSourceImpl(apiService);
+  when(popularPersonsImpl.fetchPopularPersons())
+      .thenAnswer((_) async => fakePopularPersonsApiResponse);
   test(
     "Verify api called",
-    () => verify(popularPersonsImpl.fetchPopularPersons()),
+    () async {
+      await popularPersonsImpl.fetchPopularPersons();
+      verify(popularPersonsImpl.fetchPopularPersons());
+    },
+  );
+  test(
+    "should return instance of PopularPersonsApiResponse",
+    () async {
+      final result = await popularPersonsImpl.fetchPopularPersons();
+      expect(result.results!.length, 0);
+    },
   );
 }
